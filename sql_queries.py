256ef530f0338f259a2dec5bf651511db1e9c779
Leon Siegner <Leon-Marco.siegner@web.de>
2024-03-03 22:38:56 +0100

    removed json code

--- sql_queries.py
+++ sql_queries.py
@@ -16,27 +16,6 @@ artist_table_drop = "DROP TABLE artist"
 time_table_drop = "DROP TABLE time"
 
 # CREATE TABLES
-{
-    "artist": null,
-    "auth": "Logged In",
-    "firstName": "Walter",
-    "gender": "M",
-    "itemInSession": 0,
-    "lastName": "Frye",
-    "length": null,
-    "level": "free",
-    "location": "San Francisco-Oakland-Hayward, CA",
-    "method": "GET",
-    "page": "Home",
-    "registration": 1540919166796.0,
-    "sessionId": 38,
-    "song": null,
-    "status": 200,
-    "ts": 1541105830796,
-    "userAgent": "\"Mozilla\/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit\/537.36 (KHTML, like Gecko) Chrome\/36.0.1985.143 Safari\/537.36\"",
-    "userId": "39"
-}
-
 staging_events_table_create= (""" CREATE TABLE staging_events(
                             artist VARCHAR,
                             auth VARCHAR,
@@ -59,18 +38,6 @@ staging_events_table_create= (""" CREATE TABLE staging_events(
 )
 """)
 
-{
-    "num_songs": 1,
-    "artist_id": "ARD7TVE1187B99BFB1",
-    "artist_latitude": null,
-    "artist_longitude": null,
-    "artist_location": "California - LA",
-    "artist_name": "Casual",
-    "song_id": "SOMZWCG12A8C13C480",
-    "title": "I Didn't Mean To",
-    "duration": 218.93179,
-    "year": 0
-}
 
 staging_songs_table_create = (""" CREATE TABLE staging_songs(
                             num_songs INTEGER,
